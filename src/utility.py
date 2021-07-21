8e237f62fda31b97e880c57fdf881eed0ccfdfa1
m r <a.m.rasouli.n@gmail.com>
2021-07-21 09:45:23 +0430

    Added validate_field_type_input to utility

--- src/utility.py
+++ src/utility.py
@@ -1,3 +1,5 @@
+from static_data.model_builder_statics import DJANGO_FIELDS, \
+                                        DJANGO_RELATION_FIELDS
 
 def show_error():
     print("\033[1;31mError:\033[0;0m ", end="")
@@ -13,4 +15,7 @@ def validate_name(name):
             return False
 
     return False if name[0].isdigit() else True
-    
+
+def validate_field_type_input(user_input):
+    length = len(DJANGO_FIELDS) + len(DJANGO_RELATION_FIELDS) - 1
+    return user_input.isdigit() and 0 <= int(user_input) <= length

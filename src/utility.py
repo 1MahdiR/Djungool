8e7e3d6b948b8a02055988c946c307434c689139
m r <a.m.rasouli.n@gmail.com>
2021-07-21 09:42:11 +0430

    Added validate_name to utility

--- src/utility.py
+++ src/utility.py
@@ -4,3 +4,13 @@ def show_error():
 
 def show_success():
     print("\033[1;32mSuccess:\033[0;0m ", end="")
+
+def validate_name(name):
+    valid_letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
+
+    for i in name:
+        if i not in valid_letters:
+            return False
+
+    return False if name[0].isdigit() else True
+    

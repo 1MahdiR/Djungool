ce9c3f8148aa8d5d3bd6eeecf6c8a886554cbc3a
m r <a.m.rasouli.n@gmail.com>
2021-07-21 11:22:12 +0430

    create_new_model is functional

--- src/main.py
+++ src/main.py
@@ -21,6 +21,40 @@ if os.name == 'nt':
 
 MODELS = list()
 
+def create_new_model():
+    model_name = input(CREATE_MODEL_NAME_PROMPT)
+
+    if not model_name: # If name isn't empty
+        print("Model name should contain at least a character!")
+        print("Aborted!\n")
+        return False
+
+    if validate_name(model_name) == False: # If name isn't validated return to main menu
+        show_error()
+        print("Model name should match Python3 naming conventions!")
+        print("Aborted!\n")
+        return False
+
+    for item in MODELS:
+        if model_name == item.name:
+            show_error()
+            print("This model is already created!")
+            print("Aborted!\n")
+            return False
+
+    new_model = Model_Builder(model_name)
+    print()
+    show_success()
+    print("Created model '{}'\n".format(model_name))
+
+    MODELS.append(new_model)
+
+def export_models():
+    pass
+
+def select_model(index):
+    pass
+
 def main_menu():
     valid_inputs = ['c', 'e', 'q']
     MODELS_INDICES = map(str, range(len(MODELS)))
@@ -32,7 +66,7 @@ def main_menu():
 
         if user_input in valid_inputs:
             if user_input == 'c': # create new model
-                pass
+                create_new_model()
             elif user_input == 'e': # export models.py
                 pass
             elif user_input == 'q': # exit from main_menu

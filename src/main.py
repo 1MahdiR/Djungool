002c3ed1b9db66b4dc609f6454c7bc7823e7948d
m r <a.m.rasouli.n@gmail.com>
2021-07-21 10:17:33 +0430

    Added main.py with a different approach, added main_menu

---
+++ src/main.py
@@ -0,0 +1,51 @@
+#
+# Django-modeler v0.2.3
+# By Ray (__mr__)
+#
+
+import sys
+import os
+
+from menu_module import *
+from field_menu import *
+from utility import *
+from static_data.prompts import MAIN_PROMPT, \
+                            CREATE_MODEL_NAME_PROMPT, \
+                            ADD_FIELD_TYPE_PROMPT, \
+                            ADD_FIELD_NAME_PROMPT
+from static_data.meta import *
+
+# Setting up colors for windows os
+if os.name == 'nt':
+    os.system("color")
+
+MODELS = list()
+
+def main_menu():
+    valid_inputs = ['c', 'e', 'q']
+    MODELS_INDICES = map(str, range(len(MODELS)))
+    valid_inputs.extend(MODELS_INDICES)
+
+    while True:
+        show_main_menu(MODELS)
+        user_input = input(MAIN_PROMPT).lower()
+
+        if user_input in valid_inputs:
+            if user_input == 'c':
+                pass
+            elif user_input == 'e':
+                pass
+            elif user_input == 'q':
+                return
+            else:
+                pass
+
+if __name__ == '__main__':
+    print(BANNER)
+    try:
+        main_menu()
+
+    except KeyboardInterrupt:
+        pass
+        
+    print("\nBye")

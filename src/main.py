28310f62627056c23f632a68bcfbdbd0c8ddff10
m r <a.m.rasouli.n@gmail.com>
2021-07-23 23:02:28 +0430

    Added select_model menu

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 #
-# Django-modeler v0.2.3
+# Django-modeler v0.3.2
 # By Ray (__mr__)
 #
 
@@ -52,8 +52,40 @@ def create_new_model():
 def export_models():
     pass
 
-def select_model(index):
+def add_field():
     pass
+
+def delete_model():
+    pass
+
+def show_model():
+    pass
+
+def delete_field():
+    pass
+
+def select_model(index):
+    model = MODELS[index]
+    valid_inputs = ['a','d','r','p','q']
+    FIELDS_INDICES = map(str, range(len(model.fields)))
+    valid_inputs.extend(FIELDS_INDICES)
+
+    while True:
+        show_model_menu(model)
+        user_input = input(MAIN_PROMPT).lower()
+        if user_input in valid_inputs:
+            if user_input == 'a':   # add new field
+                pass
+            elif user_input == 'd': # delete model
+                pass
+            elif user_input == 'r': # rename model
+                pass
+            elif user_input == 'p': # show model
+                pass
+            elif user_input == 'q': # return to main menu
+                return
+            else:                   # delete field
+                pass
 
 def main_menu():
     valid_inputs = ['c', 'e', 'q']
@@ -65,13 +97,13 @@ def main_menu():
         user_input = input(MAIN_PROMPT).lower()
 
         if user_input in valid_inputs:
-            if user_input == 'c': # create new model
+            if user_input == 'c':   # create new model
                 create_new_model()
             elif user_input == 'e': # export models.py
                 pass
             elif user_input == 'q': # exit from main_menu
                 return
-            else: # selecting models
+            else:                   # selecting models
                 pass
 
 if __name__ == '__main__':
--- src/static_data/meta.py
+++ src/static_data/meta.py
@@ -1,2 +1,2 @@
-VERSION = "v0.3.0"
+VERSION = "v0.3.2"
 BANNER = "\nDjango-modeler (%s)\nWritten by Ray (__mr__)\n\n" % VERSION

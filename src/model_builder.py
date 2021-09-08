482138fdd0d714d25f70af7ed4dbd8bb8352136f
m r <a.m.rasouli.n@gmail.com>
2021-09-08 23:45:46 +0430

    Added choices option to IntegerField_Builder

--- src/model_builder.py
+++ src/model_builder.py
@@ -543,10 +543,11 @@ class ImageField_Builder(Field_Builder):
 
 class IntegerField_Builder(Field_Builder):
 
-    def __init__(self,name,default=None,blank=False,null=False,unique=False):
+    def __init__(self,name,default=None,choices=None,blank=False,null=False,unique=False):
         self._name = name
         self._field = "models.IntegerField"
         self._default = default
+        self._choices = choices
         self._blank = blank
         self._null = null
         self._unique = unique
@@ -567,6 +568,10 @@ class IntegerField_Builder(Field_Builder):
         if self._default:
             default = "default={},".format(self._default)
 
+        choices = ""
+        if self._choices:
+            choices = "choices={},".format(self._choices.get_name())
+
         blank = ""
         if self._blank:
             blank = "blank={},".format(self._blank)
@@ -579,8 +584,8 @@ class IntegerField_Builder(Field_Builder):
         if self._unique:
             unique = "unique={}".format(self._unique)
 
-        param = "{default}{blank}{null}{unique}".format(
-                default=default, blank=blank, null=null, unique=unique)
+        param = "{default}{choices}{blank}{null}{unique}".format(
+                default=default, choices=choices, blank=blank, null=null, unique=unique)
 
         if param and param[-1] == ",":
             param = param[:-1]
